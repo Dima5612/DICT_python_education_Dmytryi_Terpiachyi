43845553cc7be2f5b1602e2134b446f517e23ea7
Ім'я корTerpiachyiDmytro519 <d.h.terpiachyi@student.khai.edu>
2022-10-08 13:19:24 +0300

    ChatBot {2-nd} stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,2 +1,5 @@
-print ("Hello! My name is bot_Baton")
-print ("I was created in 2022")
+print("Hello! My name is bot_Baton")
+print("I was created in 2022")
+print("Please, remind me your name")
+name = input(">")
+print(f"What a great name you have,{name}!")
