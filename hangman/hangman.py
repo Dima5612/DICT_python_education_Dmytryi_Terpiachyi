b3fae6901d617c144cef7e6dff40299c4cbbd96d
Ім'я корTerpiachyiDmytro519 <d.h.terpiachyi@student.khai.edu>
2022-10-15 13:27:55 +0300

    hangman 4-th stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -13,3 +13,9 @@ while True:
         print("You lost")
         break
 # 2-nd stage completed
+lenght = '_'*len(word)
+a = input("Guess the word :" + word [:3] + "-".join([''for _ in range (len(word)-3)])+ "-\n>")
+if a == word:
+    print("You survive!")
+else:
+    print("You lose!")
