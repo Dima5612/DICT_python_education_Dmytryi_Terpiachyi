d0eae65e003d005ef55b94314b6b2526f5530fac
Ім'я корTerpiachyiDmytro519 <d.h.terpiachyi@student.khai.edu>
2022-10-22 13:22:37 +0300

    dinnerparty 2-nd stage

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,11 +1,14 @@
 number =int(input("Enter the number of friends joining (including you):\n>"))
-print(number)
 if number <= 0:
     print('No one is joining for the party.')
     exit ()
 Debtors = {}
 print('Enter the name of every friend (including you), each on a new line:')
 for _ in range(number):
-    name = input(">")
+    name = input("-")
     Debtors[name] = 0
+duty = int(input('Enter the total amount:\n>'))
+a = round(duty/number, 2)
+for name in Debtors:
+    Debtors[name] = a
 print(Debtors)
