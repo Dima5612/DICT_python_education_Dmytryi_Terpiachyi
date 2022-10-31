944a18c9249a30375032b30a4677c63abef5ec61
Ім'я корTerpiachyiDmytro519 <d.h.terpiachyi@student.khai.edu>
2022-10-31 17:40:17 +0200

    tictactoe {2-nd} stage

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,1 +1,12 @@
-print ( "XOX \ nO XO \ nXXO " )
+theBoard = {'1,1': ' ', '1,2': ' ', '1,3': ' ',
+            '2,1': ' ', '2,2': ' ', '2,3': ' ',
+            '3,1': ' ', '3,2': ' ', '3,3': ' '}
+
+def printBoard(board):
+    print('------')
+    print(board['1,1'] + '|' + board['1,2'] + '|' + board['1,3'])
+    print('-+-+-')
+    print(board['2,1'] + '|' + board['2,2'] + '|' + board['2,3'])
+    print('-+-+-')
+    print(board['3,1'] + '|' + board['3,2'] + '|' + board['3,3'])
+    print('------')
