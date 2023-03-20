f598c2a1c8253fb018e1d426777027bde0d0ed65
Ім'я корTerpiachyiDmytro519 <d.h.terpiachyi@student.khai.edu>
2023-03-20 16:57:48 +0200

    arithmetic_test {2-nd} stage

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,30 +1,51 @@
 import random
 
-num_list = [2, 3, 4, 5, 6, 7, 8, 9]
-sign_list = ['+', '-', '*']
+print("Hello user \n Power by Stell" )
 
-a = random.choice(num_list)
-b = random.choice(num_list)
-sign = random.choice(sign_list)
-print(a, sign, b)
+def generate_question():
+    num1 = random.randint(2, 9)
+    num2 = random.randint(2, 9)
+    operator = random.choice(["+", "-", "*"])
+    question = f"{num1} {operator} {num2} = "
+    if operator == "+":
+        answer = num1 + num2
+    elif operator == "-":
+        answer = num1 - num2
+    else:
+        answer = num1 * num2
+    return question, answer
+def main():
+    question, answer = generate_question()
+    print(question)
+    user_answer = int(input())
+    if user_answer == answer:
+        print("Right!")
+    else:
+        print("Wrong!")
+def check_input(user_input):
+    try:
+        int(user_input)
+        return True
+    except ValueError:
+        print("Incorrect format.")
+        return False
 
-Input = int(input(">"))
-addition = 0
-subtraction = 0
-multiplication = 0
-
-if sign == "+":
-    addition = int(a) + int(b)
-elif sign == "-":
-    subtraction = int(a) - int(b)
-elif sign == "*":
-    multiplication = int(a) * int(b)
-
-if Input == addition:
-    print("Right!")
-elif Input == subtraction:
-    print("Right!")
-elif Input == multiplication:
-    print("Right!")
-else:
-    print("Wrong")
+def main():
+    num_correct = 0
+    for i in range(5):
+        question, answer = generate_question()
+        print(question)
+        while True:
+            user_answer = input()
+            if check_input(user_answer):
+                break
+        if int(user_answer) == answer:
+            print("Right!")
+            num_correct += 1
+        else:
+            print("Wrong!")
+    #  кількість правильних відповідей
+    print(f"Your mark is {num_correct}/5.")
+    print("Goodbye \n Have a nice day")
+if __name__ == '__main__':
+    main()
