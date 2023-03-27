1d3bbf6a7361e62924bc142bcf2ca7993a5857c5
Ім'я корTerpiachyiDmytro519 <d.h.terpiachyi@student.khai.edu>
2023-03-27 13:59:46 +0300

    arithmetic_test {3} stage

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,51 +1,61 @@
 import random
-
-print("Hello user \n Power by Stell" )
-
-def generate_question():
-    num1 = random.randint(2, 9)
-    num2 = random.randint(2, 9)
-    operator = random.choice(["+", "-", "*"])
-    question = f"{num1} {operator} {num2} = "
-    if operator == "+":
-        answer = num1 + num2
-    elif operator == "-":
-        answer = num1 - num2
+levels = ["simple operations with numbers 2-9", "integral squares of 11-29"]
+choices = ["yes", "YES", "y", "Yes"]
+print("Which level do you want? Enter a number:")
+print("1 - simple operations with numbers 2-9")
+print("2 - integral squares of 11-29")
+ex_choice = 0
+choice = int(input())
+mark = 0
+while choice != 1 or choice != 2:
+    if choice == 1:
+        for _ in range(5):
+            operations = [" + ", " - ", " * "]
+            question = str(random.randint(2, 9)) + random.choice(operations) + str(random.randint(2, 9))
+            print(question)
+            while True:
+                try:
+                    Input = int(input())
+                    if Input == eval(question):
+                        print("Right!")
+                        mark += 1
+                    else:
+                        print("Wrong!")
+                    break
+                except ValueError:
+                    print("Incorrect format.")
+                    continue
+        break
+    elif choice == 2:
+        for _ in range(5):
+            question = int(random.randint(11, 29))
+            print(question)
+            while True:
+                try:
+                    Input = int(input())
+                    if Input == question ** 2:
+                        print("Right!")
+                        mark += 1
+                    else:
+                        print("Wrong!")
+                    break
+                except ValueError:
+                    print("Incorrect format.")
+                    continue
+        break
     else:
-        answer = num1 * num2
-    return question, answer
-def main():
-    question, answer = generate_question()
-    print(question)
-    user_answer = int(input())
-    if user_answer == answer:
-        print("Right!")
-    else:
-        print("Wrong!")
-def check_input(user_input):
-    try:
-        int(user_input)
-        return True
-    except ValueError:
         print("Incorrect format.")
-        return False
-
-def main():
-    num_correct = 0
-    for i in range(5):
-        question, answer = generate_question()
-        print(question)
-        while True:
-            user_answer = input()
-            if check_input(user_answer):
-                break
-        if int(user_answer) == answer:
-            print("Right!")
-            num_correct += 1
-        else:
-            print("Wrong!")
-    #  кількість правильних відповідей
-    print(f"Your mark is {num_correct}/5.")
-    print("Goodbye \n Have a nice day")
-if __name__ == '__main__':
-    main()
+        print("Which level do you want? Enter a number:")
+        print("1 - simple operations with numbers 2-9")
+        print("2 - integral squares of 11-29")
+        choice = int(input())
+print("Your mark is {}/5. Would you like to save the result? Enter yes or no.".format(mark))
+choice2 = input().strip()
+if choice2 in choices:
+    name = input("What is your name?\n")
+    ex_choice = choice - 1
+    selected_level = levels[ex_choice]
+    s = "{}: {}/5 in level {} ({}).".format(name, mark, choice, selected_level)
+    with open("results.txt", "a") as f:
+        f.write(s)
+    print('The results are saved in "results.txt".')
